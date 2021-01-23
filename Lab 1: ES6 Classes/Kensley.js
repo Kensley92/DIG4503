28be2f616aa7d40a51002ebe771c17a1c1e0caf6
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-01-22 21:41:46 -0500

    Create Kensley.js

---
+++ Lab 1: ES6 Classes/Kensley.js
@@ -0,0 +1,9 @@
+const Person = require("./Person.js");
+
+class Kensley extends Person {
+    constructor(name, colors, bgColors) {
+        super(name, colors, bgColors);
+    }
+}
+
+module.exports = Kensley;
