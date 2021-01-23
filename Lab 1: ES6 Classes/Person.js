afdbc725257520148c944f458f5fb62ca0a9f471
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-01-22 21:42:02 -0500

    Create Person.js

---
+++ Lab 1: ES6 Classes/Person.js
@@ -0,0 +1,13 @@
+const chalk = require("chalk");
+class Person {
+    constructor(name, favoriteColors, backgroundColors) {
+        this.name = name;
+        this.favoriteColors = favoriteColors;
+           }
+    speak() {
+        for(let i = 0; i < this.favoriteColors.length; i++) {
+            console.log(chalk.hex(this.favoriteColors[i])(this.name));
+        }
+    }
+}
+module.exports = Person;
