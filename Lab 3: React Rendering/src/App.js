6b167244743ae6094c6f0b847f9c1a22b1cf796c
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-02-05 13:11:12 -0500

    Add files via upload

---
+++ Lab 3: React Rendering/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './homepage'
+
+function App() {
+  const frame = 'Kensley';
+  return (
+    <HomePage firstname={fname} />
+  )
+}
+
+export default App;
